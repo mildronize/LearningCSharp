325b1b583c15a8ccf0b1e3825a5ed54cfcffe06b
Thada Wangthammang <thada.wth@gmail.com>
2024-09-25 21:48:13 +0700

    add solution of TwoSum

--- TwoSum/Solution.cs
+++ TwoSum/Solution.cs
@@ -5,7 +5,16 @@ public class Solution
 {
     public int[] TwoSum(int[] nums, int target)
     {
-        Console.WriteLine("Hello World!");
+        for(var i = 0 ; i < nums.Length; i++)
+        {
+            for(var j = i + 1; j < nums.Length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    return new int[] {i, j};
+                }
+            }
+        }
         return [];
     }
 }
